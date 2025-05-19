a81f83bd0986264f87b1b11b9de7abbd7c292a4a
Debajit Kataki <debajitkataki@gmail.com>
2025-05-19 20:53:51 +0530

    adding my refined install steps for Prometheus and Grafana

--- main.tf
+++ main.tf
@@ -2,11 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_key_pair" "my_key" {
+resource "aws_key_pair" "tf-user" {
   key_name   = "tf-user"
   public_key = file("~/.ssh/id_rsa.pub")
 }
-
 
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
@@ -17,7 +16,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["202.181.129.18/32"] # Replace YOUR_IP with your real IP
+    cidr_blocks = ["0.0.0.0/32"] # Replace with your IP
   }
 
   ingress {
@@ -37,9 +36,9 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 resource "aws_instance" "prometheus" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 in us-east-1
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
   instance_type = "t2.micro"
-  key_name = aws_key_pair.tf_user.key_name
+  key_name      = aws_key_pair.tf-user.key_name
   security_groups = [aws_security_group.allow_ssh.name]
 
   tags = {
@@ -53,10 +52,11 @@ resource "aws_instance" "prometheus" {
               EOF
 }
 
+# ✅ UPDATED: Grafana instance now installs Grafana OSS and starts the service
 resource "aws_instance" "grafana" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
-  key_name = aws_key_pair.tf_user.key_name
+  key_name      = aws_key_pair.tf-user.key_name
   security_groups = [aws_security_group.allow_ssh.name]
 
   tags = {
@@ -66,7 +66,26 @@ resource "aws_instance" "grafana" {
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
-              yum install wget -y
+              yum install -y wget
+              cat <<EOL > /etc/yum.repos.d/grafana.repo
+              [grafana]
+              name=grafana
+              baseurl=https://packages.grafana.com/oss/rpm
+              repo_gpgcheck=1
+              enabled=1
+              gpgcheck=1
+              gpgkey=https://packages.grafana.com/gpg.key
+              sslverify=1
+              EOL
+              yum install -y grafana
+              systemctl daemon-reexec
+              systemctl enable grafana-server
+              systemctl start grafana-server
               EOF
 }
 
+# ✅ NEW: Output to fetch the public IP of the Grafana instance
+output "grafana_public_ip" {
+  value = aws_instance.grafana.public_ip
+  description = "Public IP to access Grafana UI on port 3000"
+}
